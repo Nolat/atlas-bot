0cf72c1a332347c6bf9ce16049acade58d9d10d1
Nolat <NolatYT@gmail.com>
2020-02-16 16:37:23 +0100

    :ambulance: Add express to prevent crash on heroku

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import "reflect-metadata";
 import "cross-fetch/polyfill";
 import "lib/env";
+import express from "express";
 
 // * CommandHandler
 import { CommandHandler } from "utils/commandHandler";
 
 // * Discord client
 import { client } from "utils/discordClient";
+
+// * Environment variables
+const PORT: number = parseInt(process.env.PORT!, 10);
 
 const initApp = () => {
   // * Create new command handler
@@ -19,6 +23,14 @@ const initApp = () => {
   // * Start Discord client
   client.start(commandHandler);
   console.log(`🤖 Discord client is online.`);
+
+  // * Create express app
+  const app = express();
+
+  // * Boot server
+  app.listen({ port: PORT }, () =>
+    console.log(`🚀 Server ready at http://localhost:${PORT}`)
+  );
 };
 
 initApp();
