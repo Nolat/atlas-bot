6708d1c378700a6b848e20df36ba27ae4a8890eb
Nolat <NolatYT@gmail.com>
2020-02-23 09:54:02 +0100

    WIP

--- src/commands/user/money/addMoney.ts
+++ src/commands/user/money/addMoney.ts
@@ -3,7 +3,7 @@ import { Message, RichEmbed, MessageMentions } from "discord.js";
 // * GraphQL
 import client from "graphql/client";
 
-import addMoney from "graphql/user/mutation/addMoney"
+import addMoney from "graphql/user/mutation/addMoney";
 
 // * Helper
 import getParamFromResponse from "helpers/discord/getParamFromResponse";
@@ -11,10 +11,10 @@ import getParamFromResponse from "helpers/discord/getParamFromResponse";
 // * Types
 import { Command } from "types";
 
-import { OperationVariables } from "apollo-boost";
-
-import { GiveUserMoneyMutation, GiveUserMoneyMutationVariables } from "generated/graphql";
-
+import {
+  GiveUserMoneyMutation,
+  GiveUserMoneyMutationVariables
+} from "generated/graphql";
 
 //* Constants
 const MONEY_QUESTION = ":moneybag: Gain d'argent";
@@ -24,38 +24,38 @@ const HOW_QUESTION = "Combien avez vous gagner";
 const addMoneyCommand: Command = {
   name: "addMoney",
   aliases: ["a$"],
-  usage:"",
+  usage: "",
   description: "donne l'argent",
   onlyStaff: true,
-  run: (message: Message) => runAddMoney(message) 
+  run: (message: Message) => runAddMoney(message)
 };
 
 const runAddMoney = async (message: Message) => {
   const embed: RichEmbed = new RichEmbed();
 
   try {
-
-
-    const id = await getParamFromResponse(message,
-      `${MONEY_QUESTION}-${id}`,
+    const id = await getParamFromResponse(
+      message,
+      `${MONEY_QUESTION}`,
       WHO_QUESTION,
       60000
     );
 
-    const money = await getParamFromResponse(message,
-      `${MONEY_QUESTION}-${Number}`,
-      HOW_QUESTION,
-      6000
+    const money = await Number(
+      getParamFromResponse(message, `${MONEY_QUESTION}`, HOW_QUESTION, 6000)
     );
+
     const { data, errors } = await client.mutate<
       GiveUserMoneyMutation,
-      GiveUserMoneyMutationVariables,
+      GiveUserMoneyMutationVariables
     >({
       mutation: addMoney,
-      variables: { id, money},
+      variables: { id, money },
       errorPolicy: "all"
     });
-  },
+  } catch (error) {
+    console.log(error);
+  }
   message.channel.send(embed);
 };
 export default addMoneyCommand;
