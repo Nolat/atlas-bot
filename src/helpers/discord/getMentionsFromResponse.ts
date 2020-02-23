34b5a27d559388239b09ec721e37f2594be466f6
Seynox <50173494+Seynox@users.noreply.github.com>
2020-02-23 10:13:51 +0100

    :sparkles: (AT-33): Add _addXP command

---
+++ src/helpers/discord/getMentionsFromResponse.ts
@@ -0,0 +1,33 @@
+import { Message, RichEmbed, MessageMentions } from "discord.js";
+import moment from "moment";
+
+const getMentionsFromResponse = async (
+  message: Message,
+  title: string,
+  question: string,
+  time: number
+): Promise<MessageMentions> => {
+  const { channel, author } = message;
+
+  const displayTime = moment.duration(time, "ms").asMinutes();
+
+  const embed: RichEmbed = new RichEmbed()
+    .setTitle(title)
+    .setDescription(question)
+    .addField("Temps", `${displayTime}min`)
+    .setColor("GOLD");
+
+  await channel.send(embed);
+
+  const collected = await channel.awaitMessages(
+    m => m.author.id === author.id,
+    {
+      max: 1,
+      time
+    }
+  );
+
+  return collected.first().mentions;
+};
+
+export default getMentionsFromResponse;
